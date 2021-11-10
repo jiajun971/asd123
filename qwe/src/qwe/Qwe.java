63cc0c27317abb555ec239449f7b65a4b1deecd7
jiajun971 <44252404+jiajun971@users.noreply.github.com>
2021-11-10 18:04:01 +0800

    Add files via upload

---
+++ qwe/src/qwe/Qwe.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qwe;
+
+/**
+ *
+ * @author User
+ */
+public class Qwe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.printf("Hello World\n");
+        System.out.printf("Hello World\n");
+        System.out.printf("Hello World\n");
+    }
+    
+}
